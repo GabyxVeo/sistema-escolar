2e1e678a6fc601c61b36b2f5f3ffffe85c517fb8
Gabyx <aldo.portillo69686@uped.edu.sv>
2026-04-20 15:59:47 -0600

    Implementacion de PWA

---
+++ public/sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'sistema-escolar-v1';
+const urlsToCache = ['/', '/index.html', '/favicon.svg'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((names) => Promise.all(
+      names.filter((n) => n !== CACHE_NAME).map((n) => caches.delete(n))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
